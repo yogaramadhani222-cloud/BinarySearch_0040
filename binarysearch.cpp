8cf9790874c592655ea9fd2e50c28c8841fc4298
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-04-16 15:49:49 +0700

    membuat output maksimal perintah elemen

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,2 +1,33 @@
 #include<iostream>
 using namespace std;
+
+int element[10];
+int npanjang;
+int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "masukkan banyaknya elemen pada array(maksimal 10): ";
+        cin >> npanjang ;
+
+        if (npanjang<=10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. silahkan coba lagi.\n";
+        }
+    }
+    cout <<"\n==============================\n";
+    cout <<"       Masukan Elemen Array      \n";
+    cout <<"\n==============================\n";
+    
+    for(int i = 0; i < npanjang;i++)
+    {
+        cout << "data ke -" << (i+1)<< " = ";
+        cin >> element[i];
+    }
+}
