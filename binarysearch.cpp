28c2fa7da71dcf16b3d32397a971b82f3e72d05b
yogaramadhani222-cloud <yogaramadhani222@gmail.com>
2026-04-16 15:55:42 +0700

    menambahkan tampilan pilihan

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -4,3 +4,21 @@ using namespace std;
 int element[10];
 int npanjang;
 int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "masukkan banyaknya elemen pada array(maksimal 10): ";
+        cin >> npanjang ;
+
+        if (npanjang<=10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak boleh lebih dari 10. silahkan coba lagi.\n";
+        }
+    }
+}
